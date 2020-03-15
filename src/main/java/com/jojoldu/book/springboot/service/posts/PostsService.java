2f09438a2dcf8cc47b6d85fecff84ca81cbbe2a9
공정훈 <rhdtl78@gmail.com>
2020-03-15 16:37:10 +0900

    나머지 api도 작성

--- src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
@@ -2,8 +2,11 @@ package com.jojoldu.book.springboot.service.posts;
 
 import javax.transaction.Transactional;
 
+import com.jojoldu.book.springboot.domain.posts.Posts;
 import com.jojoldu.book.springboot.domain.posts.PostsRepository;
+import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
+import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 
 import org.springframework.stereotype.Service;
 
@@ -17,5 +20,22 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                                    .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                                    .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        
+        return new PostsResponseDto(entity);
     }
 }
