43871bdcb918434621406fc2455dfd592c8accc7
공정훈 <rhdtl78@gmail.com>
2020-03-15 17:03:52 +0900

    JPA Auditing으로 createdDate, modifiedDate를 모든 entity에 추가

--- src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -8,12 +8,14 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Id; 
+import javax.persistence.Id;
+
+import com.jojoldu.book.springboot.domain.BaseTimeEntity; 
 
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
