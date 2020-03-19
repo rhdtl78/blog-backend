b08b37db5be603de4bc62a28a1f2da8a6742d54f
공정훈 <rhdtl78@gmail.com>
2020-03-19 14:46:47 +0900

    세션 정보 조회 기능을 어노테이션으로 구현

---
+++ src/main/java/com/jojoldu/book/springboot/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.jojoldu.book.springboot.config;
+
+import java.util.List;
+
+import com.jojoldu.book.springboot.config.auth.LoginUserArgumentResolver;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ * WebConfig
+ */
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginUserArgumentResolver);
+    }
+}
