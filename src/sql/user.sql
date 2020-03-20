2cd7f6c4632765727035bb4f774e5c7f2c8ad276
공정훈 <rhdtl78@gmail.com>
2020-03-20 17:27:07 +0900

    real 설정 추가

---
+++ src/sql/user.sql
@@ -0,0 +1,10 @@
+create table user (
+    id bigint not null auto_increment, 
+    created_date datetime, 
+    modified_date datetime, 
+    email varchar(255), 
+    name varchar(255) not null, 
+    picture varchar(255), 
+    role varchar(255) not null, 
+    primary key (id)
+) engine=InnoDB
